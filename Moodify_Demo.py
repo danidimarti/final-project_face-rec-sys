240d69801d941e10e4a7dcdf0d550a7f55a99d11
danidimarti <dani.dimarti@gmail.com>
2023-03-14 17:24:19 +0100

    scaling spotify recomendations DONE!

--- Moodify_Demo.py
+++ Moodify_Demo.py
@@ -13,11 +13,19 @@ import os
 import statistics
 import webbrowser
 import detector as dt
+import getsong as gs
+
+CLIENT_ID = os.environ.get('CLIENT_ID')
+CLIENT_SECRET = os.environ.get('CLIENT_SECRET')
 
 with open('style.css') as f: 
     st.markdown(f'<style>{f.read()} </style>', unsafe_allow_html=True)
 
 st.image('imgs/streamlit-header_temp.png', use_column_width=True)
+
+df = pd.read_csv('Spotify/data/kaggle_song-features.csv')
+seed_song = df.sample()
+
 
 if st.button('Moodify me', key="center_button"):
     emotion = dt.detect_emotion()
@@ -36,7 +44,7 @@ if st.button('Moodify me', key="center_button"):
     subset = dt.subsets['sub_' + emotion]
     mood_result = subset.sample()
     link = mood_result['track_link'].iloc[0]
-    
+    link_2 = gs.getsong(seed_song)
     # Use st.empty() to create a placeholder for the countdown
     countdown_text = st.empty()
     
@@ -48,7 +56,7 @@ if st.button('Moodify me', key="center_button"):
     countdown_text.empty()
     
     # Open the link
-    webbrowser.open_new_tab(link)
+    webbrowser.open_new_tab(link_2)
  
     
     # Add the footer text at the bottom of the page
